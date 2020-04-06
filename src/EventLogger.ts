02deeea86c564ac14b8f9a52b608147e78391c46
Guido Doumen <guido.doumen@vodafone.com>
2020-04-06 13:48:57 +0200

    Empty Classdefs

---
+++ src/EventLogger.ts
@@ -0,0 +1,11 @@
+import Context from './Context'
+
+export default class EventLogger  {
+    private context: Context;
+
+    constructor (context:Context) {
+        this.context = context;
+    }
+
+
+}
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import * as EventLogger from './EventLogger'
+import * as Context from './Context'
+
+export {
+    EventLogger,
+    Context
+}
