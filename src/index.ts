7467c3c89e9579baef1de38e8eeb21037140e252
Guido <guido.doumen@vodafone.com>
2020-04-08 21:57:51 +0200

    bugifx: missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import EventLogger from './EventLogger'
 import Context from './Context'
 import LogAdapter from './LogAdapter'
+import BaseAdapter from './Adapters/BaseAdapter'
 import ConsoleAdapter from './Adapters/ConsoleAdapter'
+import FileAdapter from './Adapters/FileAdapter'
 
 
 export  {
     EventLogger,
     Context,
     LogAdapter,
-    ConsoleAdapter
+    BaseAdapter,
+    ConsoleAdapter,
+    FileAdapter
 }
 
